9dc71c0daed5e8baaea05277ce6caf9ffc8e4cff
Berhe Tekeste <12522087+btekeste@users.noreply.github.com>
2022-02-01 22:20:34 -0500

    Update AssetList.js

--- src/components/AssetList.js
+++ src/components/AssetList.js
@@ -7,17 +7,20 @@ export const AssetList = () => {
 
 	return (
 		<>
-			<h3>List of Assets</h3>
-			<table>
-				<tbody className="table">
+			<h3 className="p-2 m-2 text-2xl text-center">List of Assets</h3>
+			<table className="table-auto border-collapse border border-slate-400 items-center">
+				<tbody>
 					<tr>
-						<th>DELETE</th>
-						<th>ID</th>
-						<th>TICKER</th>
-						<th>DESCRIPTION</th>
-						<th>LAST PRICE</th>
-						<th>CHANGE PERCENTAGE</th>
-						<th>CHANGE VALUE</th>
+						<TailwindCSSTableTitle>REMOVE</TailwindCSSTableTitle>
+						<TailwindCSSTableTitle>ID</TailwindCSSTableTitle>
+						<TailwindCSSTableTitle>TICKER</TailwindCSSTableTitle>
+						<th className="hidden md:table-cell w-auto border border-slate-300 dark:border-slate-600 
+							font-semibold text-sm m-1 md:p-2 text-slate-900 dark:text-slate-200 text-center">
+							DESCRIPTION
+						</th>
+						<TailwindCSSTableTitle>LAST PRICE</TailwindCSSTableTitle>
+						<TailwindCSSTableTitle>CHANGE PERCENTAGE</TailwindCSSTableTitle>
+						<TailwindCSSTableTitle>CHANGE VALUE</TailwindCSSTableTitle>						
 					</tr>	
 					{assets.map(asset =>
 						(<Asset key={ asset.id } asset={ asset } />))				
@@ -26,4 +29,11 @@ export const AssetList = () => {
 			</table>
 		</>
     )
+}
+
+const TailwindCSSTableTitle = (props) => {
+	return(
+		<th className="w-auto border border-slate-300 dark:border-slate-600 font-semibold text-sm md:m-1 
+			md:p-2 text-slate-900 dark:text-slate-200 text-center">{props.children}</th>
+	)
 }
