542384e03ebbd1618e16b03539bb6adf1bd7a283
Berhe Tekeste <12522087+btekeste@users.noreply.github.com>
2022-02-01 22:21:51 -0500

    Update Holdings.js

--- src/components/Holdings.js
+++ src/components/Holdings.js
@@ -9,9 +9,9 @@ export const Holdings = () => {
 		.reduce((previousValue, currentValue) => previousValue + currentValue);
 
     return (
-		<>
-			<h4>Holdings</h4>
-			<h1>{holdings.toLocaleString("en-US")}</h1>
-		</>
+		<div>
+			<h4 className="pt-5 text-l text-center text-gray-500">Current Holding Based on Price: 
+			<span className="text-blue-400"> {holdings.toLocaleString("en-US")}</span></h4>
+		</div>
     )
 }
